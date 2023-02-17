df6f346b8c7f108a91ca4685f1dfd9022fb170a1
SahilSandhu1233 <90067655+SahilSandhu1233@users.noreply.github.com>
2023-02-17 16:46:45 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+/*********************************************************************************
+*  WEB322 – Assignment 03
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part 
+*  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: Sahil Sandhu     Student ID: 117526210      Date: 17th Feb 2023
+*
+*  Online (Cyclic) Link: https://calm-teal-mite-gear.cyclic.app/
+*
+********************************************************************************/ 
+
 const express = require("express");
 const path = require("path");
 const data = require("./data-service.js");
